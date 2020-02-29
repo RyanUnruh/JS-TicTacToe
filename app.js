c4a0bf8a2a1db2b450b7a1513719fd441c9b98df
Ryan Unruh <ryanunruhmedia@gmail.com>
2020-02-29 17:21:56 -0600

    FEB29

--- app.js
+++ app.js
@@ -1,56 +1,62 @@
-let cells = document.querySelectorAll('.cell');
-let button = document.querySelector('#btn');
+let cells = document.querySelectorAll(".cell");
+let button = document.querySelector("#btn");
 let turnCounter = 0;
 let winCombos = [
-    [cells[0], cells[1], cells[2]],
-    [cells[3], cells[4], cells[5]],
-    [cells[6], cells[7], cells[8]],
-    [cells[0], cells[3], cells[6]],
-    [cells[1], cells[4], cells[7]],
-    [cells[2], cells[5], cells[8]],
-    [cells[2], cells[4], cells[6]],
-    [cells[0], cells[4], cells[8]]
-]
+  [cells[0], cells[1], cells[2]],
+  [cells[3], cells[4], cells[5]],
+  [cells[6], cells[7], cells[8]],
+  [cells[0], cells[3], cells[6]],
+  [cells[1], cells[4], cells[7]],
+  [cells[2], cells[5], cells[8]],
+  [cells[2], cells[4], cells[6]],
+  [cells[0], cells[4], cells[8]]
+];
 
-cells.forEach((cell) => {
-    cell.addEventListener("click", () => {
-        if (turnCounter % 2 === 0) {
-            cell.textContent = "X";
-        } else {
-            cell.textContent = "O";
-        }
-
-        turnCounter++
-        checkWin();
-    });
+cells.forEach(cell => {
+  cell.addEventListener("click", () => {
+    if (turnCounter % 2 === 0) {
+      cell.textContent = "X";
+    } else {
+      cell.textContent = "O";
+    }
+    turnCounter++;
+    // deactivate();
+    checkWin();
+  });
 });
 
-
 function check(mark) {
+  winCombos.forEach(combo => {
+    let sum = 0;
+    combo.forEach(c => {
+      if (c.textContent === mark) {
+        sum++;
+      }
 
-    winCombos.forEach((combo) => {
-        let sum = 0;
-        combo.forEach((c) => {
-            if (c.textContent === mark) {
-                sum++
-            }
-
-            if (sum === 3) {
-                alert(mark + " Wins!")
-                return;
-            }
-        });
+      if (sum === 3) {
+        alert(mark + " Wins!");
+        return;
+      }
     });
+  });
 }
 
 function checkWin() {
-    check("X");
-    check("O");
+  check("X");
+  check("O");
+}
+
+function deactivate() {
+  if ((cell.innerHTML = "X" || "O")) {
+    cell.removeEventListener(cell);
+  } else {
+    return;
+  }
 }
 
 button.addEventListener("click", () => {
-    cells.forEach((cell) => {
-        cell.innerHTML = "";
-        turnCounter = 0;
-    });
-})
+  cells.forEach(cell => {
+    cell.innerHTML = "";
+    turnCounter = 0;
+  });
+});
