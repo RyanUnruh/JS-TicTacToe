68d47c14823c4583673da7b32957f88d197c3ad6
Ryan Unruh <ryanunruhmedia@gmail.com>
2020-04-07 22:11:25 -0500

    Apr7

--- app.js
+++ app.js
@@ -12,18 +12,7 @@ let winCombos = [
   [cells[0], cells[4], cells[8]]
 ];
 
-function reset() {
-  cells.forEach(cell => {
-    cell.innerHTML = "";
-    endgame.style.display = "none";
-    turnCounter = 0;
-  });
-
-  cells.forEach(cell => {
-    cell.addEventListener("click", onClick);
-  });
-}
-
+// Determine player turn
 function onClick(event) {
   if (turnCounter % 2 === 0) {
     event.target.textContent = "X";
@@ -36,6 +25,7 @@ function onClick(event) {
   event.target.removeEventListener("click", onClick);
 }
 
+// Check win or tie
 function checkWin(mark) {
   winCombos.forEach(combo => {
     let sum = 0;
@@ -57,10 +47,25 @@ function checkWin(mark) {
   });
 }
 
-document.querySelector("#btn").addEventListener("click", reset);
-reset();
-
 function declareWinner(mark) {
   document.querySelector(".endgame").style.display = "block";
   document.querySelector(".endgame .text").innerText = mark;
 }
+
+
+// Restart button 
+document.querySelector("#btn").addEventListener("click", reset);
+reset();
+
+function reset() {
+  cells.forEach(cell => {
+    cell.innerHTML = "";
+    endgame.style.display = "none";
+    turnCounter = 0;
+  });
+
+  cells.forEach(cell => {
+    cell.addEventListener("click", onClick);
+  });
+}
+
